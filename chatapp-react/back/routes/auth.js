9dcc82712ce7a0a99ff12ef7ccaf3b42f0a82005
iamidlek <iamidlek@naver.com>
2022-03-10 21:22:41 +0900

    feat: register

--- chatapp-react/back/routes/auth.js
+++ chatapp-react/back/routes/auth.js
@@ -1,16 +1,27 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // register
-router.get("/register", async (req, res) => {
-  const user = await new User({
-    username: "test",
-    email: "test@gamil.com",
-    password: "abc123456",
-  });
+router.post("/register", async (req, res) => {
+  try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  await user.save();
-  res.send("OK");
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
