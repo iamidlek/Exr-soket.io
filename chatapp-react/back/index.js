a52130de62f7359bf3ec7a60f9b822e3b52bfa0c
iamidlek <iamidlek@naver.com>
2022-03-10 20:41:05 +0900

    feat: set users.js

--- chatapp-react/back/index.js
+++ chatapp-react/back/index.js
@@ -7,6 +7,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+
 dotenv.config();
 
 mongoose.connect(
@@ -22,13 +24,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.send("welcome to homepage");
-});
-
-app.get("/users", (req, res) => {
-  res.send("welcome to homepage");
-});
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
