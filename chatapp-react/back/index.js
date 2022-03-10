625b2db1710777b257a2888d654e625476610749
iamidlek <iamidlek@naver.com>
2022-03-10 20:38:15 +0900

    feat: set middleware

--- chatapp-react/back/index.js
+++ chatapp-react/back/index.js
@@ -17,6 +17,19 @@ mongoose.connect(
   }
 );
 
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("welcome to homepage");
+});
+
+app.get("/users", (req, res) => {
+  res.send("welcome to homepage");
+});
+
 app.listen(8800, () => {
   console.log("Backend server is running!");
 });
