808d3a4b7fe3e3fb4f2009f16dfaf4564e4b79eb
iamidlek <iamidlek@naver.com>
2022-03-11 17:23:02 +0900

    feat: create post routes

---
+++ chatapp-react/back/routes/posts.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+//create a post
+
+//update a post
+
+//delete a post
+
+//like / dislike a post
+
+//get a post
+
+//get timeline posts
+
+module.exports = router;
