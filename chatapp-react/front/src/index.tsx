39e9cfde02a603baab6d4aaeef33c5061dba8e6c
iamidlek <iamidlek@naver.com>
2022-03-12 23:11:21 +0900

    conf: eslint, prettier

--- chatapp-react/front/src/index.tsx
+++ chatapp-react/front/src/index.tsx
@@ -1,6 +1,6 @@
-import { render } from "react-dom";
-import App from "./App";
+import { render } from 'react-dom';
+import App from './App';
 
-import "./index.scss";
+import './index.scss';
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
--- chatapp-react/front/webpack.config.js
+++ chatapp-react/front/webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
   },
   module: {
     rules: [
       {
         test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "ts-loader"],
+        use: ['babel-loader', 'ts-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|webp)$/,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new Dotenv({
-      path: "./.env",
+      path: './.env',
       safe: true,
     }),
   ],
   devServer: {
-    host: "localhost",
+    host: 'localhost',
     port: 3050,
     hot: true,
   },
