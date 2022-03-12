f89e6245d2c398e81ec2e2e94efed1059b5c52b0
iamidlek <iamidlek@naver.com>
2022-03-12 22:02:49 +0900

    conf: dot env

--- chatapp-react/front/webpack.config.js
+++ chatapp-react/front/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -30,6 +31,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+    }),
+    new Dotenv({
+      path: "./.env",
+      safe: true,
     }),
   ],
   devServer: {
