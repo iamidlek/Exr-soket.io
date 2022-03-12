cbdb49dfe2a3dee99da7213a2bc5fc0800388a5d
iamidlek <iamidlek@naver.com>
2022-03-12 21:46:53 +0900

    conf: init project

---
+++ chatapp-react/front/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3050,
+    hot: true,
+  },
+};
