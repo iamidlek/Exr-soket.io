68d4ead7c4d4aa91356153715962285781070c54
iamidlek <iamidlek@naver.com>
2022-03-13 23:03:09 +0900

    feat: add sidebar components

--- chatapp-react/front/src/pages/home/Home.tsx
+++ chatapp-react/front/src/pages/home/Home.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
 import Topbar from '../../components/topbar/Topbar';
+import Sidebar from '../../components/sidebar/sidebar';
 
 const Home = () => {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <HomeContainer>
+        <Sidebar />
+      </HomeContainer>
+    </>
+  );
 };
+
+const HomeContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
 
 export default Home;
