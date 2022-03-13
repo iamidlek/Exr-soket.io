ddca06bd98e97a1e178f2c787f391bcad1a7cb85
iamidlek <iamidlek@naver.com>
2022-03-13 23:33:54 +0900

    feat: create closefriend component

---
+++ chatapp-react/front/src/components/closeFriend/CloseFriend.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export default function CloseFriend({ user }: Iprops) {
+  return (
+    <SidebarFriend>
+      <SidebarFriendImg src={user.profilePicture} alt="profilePicture" />
+      <span>{user.username}</span>
+    </SidebarFriend>
+  );
+}
+
+const SidebarFriend = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const SidebarFriendImg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 10px;
+`;
+
+interface Iprops {
+  user: {
+    id: number;
+    profilePicture: string;
+    username: string;
+  };
+}
