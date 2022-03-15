3c1132ece04b04457814a73e7d95a56d05b316f0
iamidlek <iamidlek@naver.com>
2022-03-15 20:59:42 +0900

    fix: LOGIN_FAILUR payload string

--- chatapp-react/front/src/context/interface.ts
+++ chatapp-react/front/src/context/interface.ts
@@ -16,12 +16,12 @@ export interface IUser {
 export interface InitialState {
   user: IUser | null;
   isFetching: boolean;
-  error: boolean;
+  error: boolean | string;
 }
 
 export type Action =
   | { type: 'LOGIN_START' }
   | { type: 'LOGIN_SUCCESS'; payload: IUser }
-  | { type: 'LOGIN_FAILURE' }
+  | { type: 'LOGIN_FAILURE'; payload: string }
   | { type: 'FOLLOW'; payload: string }
   | { type: 'UNFOLLOW'; payload: string };
