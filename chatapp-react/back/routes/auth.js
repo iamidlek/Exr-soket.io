953e04fd78d96b514522f3a2773e023842d0a641
iamidlek <iamidlek@naver.com>
2022-03-15 14:10:19 +0900

    fix: pw must string

--- chatapp-react/back/routes/auth.js
+++ chatapp-react/back/routes/auth.js
@@ -7,7 +7,7 @@ router.post("/register", async (req, res) => {
   try {
     //generate new password
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await bcrypt.hash(`${req.body.password}`, salt);
 
     //create new user
     const newUser = new User({
@@ -20,7 +20,7 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 });
 
@@ -31,7 +31,7 @@ router.post("/login", async (req, res) => {
     !user && res.status(404).json("user not found");
 
     const validPassword = await bcrypt.compare(
-      req.body.password,
+      `${req.body.password}`,
       user.password
     );
     !validPassword && res.status(400).json("wrong password");
