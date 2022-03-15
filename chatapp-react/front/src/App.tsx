c1c18797ae52b01e6dc7f86c853cda5c8182ff76
iamidlek <iamidlek@naver.com>
2022-03-15 20:51:48 +0900

    feat: set route

--- chatapp-react/front/src/App.tsx
+++ chatapp-react/front/src/App.tsx
@@ -1,30 +1,17 @@
-import { useContext } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthContext } from './context/AuthContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
 
 const App = () => {
-  const { user } = useContext(AuthContext);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={user ? <Home /> : <Navigate replace to="/register" />}
-        />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route
-          path="/login"
-          element={user ? <Navigate replace to="/" /> : <Login />}
-        /> */}
-        {/*
-        <Route path="/profile/:username">
-          <Profile />
-        </Route> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
