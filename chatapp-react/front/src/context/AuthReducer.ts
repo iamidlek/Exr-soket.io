db1022fce84bdb0ef8d604270395044faf7a3767
iamidlek <iamidlek@naver.com>
2022-03-15 21:01:23 +0900

    feat: create AuthReducer

---
+++ chatapp-react/front/src/context/AuthReducer.ts
@@ -0,0 +1,52 @@
+import { InitialState, Action } from './interface';
+
+const AuthReducer = (state: InitialState, action: Action): InitialState => {
+  switch (action.type) {
+    case 'LOGIN_START':
+      return {
+        user: null,
+        isFetching: true,
+        error: false,
+      };
+    case 'LOGIN_SUCCESS':
+      return {
+        user: action.payload,
+        isFetching: false,
+        error: false,
+      };
+    case 'LOGIN_FAILURE':
+      return {
+        user: null,
+        isFetching: false,
+        error: action.payload,
+      };
+    case 'FOLLOW':
+      if (state!.user!.followings?.length) {
+        return {
+          ...state,
+          user: {
+            ...state.user,
+            followings: [...state?.user!.followings, action.payload],
+          },
+        };
+      }
+      return state;
+    case 'UNFOLLOW':
+      if (state!.user!.followings?.length) {
+        return {
+          ...state,
+          user: {
+            ...state.user,
+            followings: state?.user!.followings.filter(
+              (following) => following !== action.payload,
+            ),
+          },
+        };
+      }
+      return state;
+    default:
+      return state;
+  }
+};
+
+export default AuthReducer;
