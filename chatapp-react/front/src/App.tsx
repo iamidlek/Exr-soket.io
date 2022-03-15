efb886235b12dd04772a1287f396c5a6e8a974f2
iamidlek <iamidlek@naver.com>
2022-03-15 19:16:45 +0900

    feat: use context state

--- chatapp-react/front/src/App.tsx
+++ chatapp-react/front/src/App.tsx
@@ -1,7 +1,33 @@
+import { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
 
 const App = () => {
-  return <Home />;
+  const { user } = useContext(AuthContext);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate replace to="/register" />}
+        />
+        <Route path="/register" element={<Register />} />
+        {/* <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        /> */}
+        {/*
+        <Route path="/profile/:username">
+          <Profile />
+        </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
