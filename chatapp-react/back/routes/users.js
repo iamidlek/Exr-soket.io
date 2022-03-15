522080932d5ff77a1ad2bd9c70d555456070e0c3
iamidlek <iamidlek@naver.com>
2022-03-15 14:15:04 +0900

    fix: mongodb traffic check

--- chatapp-react/back/routes/users.js
+++ chatapp-react/back/routes/users.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // update
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
-    // password 변경
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
-        req.body.password = await bcrypt.hash(req.body.password, salt);
+        req.body.password = await bcrypt.hash(`${req.body.password}`, salt);
       } catch (err) {
         return res.status(500).json(err);
       }
     }
     try {
       const user = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body, // 자동으로 모든 body를 set해줌
+        $set: req.body,
       });
       res.status(200).json("Account has been updated");
     } catch (err) {
@@ -41,11 +41,35 @@ router.delete("/:id", async (req, res) => {
 });
 
 // get a user
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
   try {
-    const user = await User.findById(req.params.id);
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
     const { password, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get friends
+router.get("/friends/:userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendList = [];
+    friends.map((friend) => {
+      const { _id, username, profilePicture } = friend;
+      friendList.push({ _id, username, profilePicture });
+    });
+    res.status(200).json(friendList);
   } catch (err) {
     res.status(500).json(err);
   }
