1a7620d7317522d63333361d64d14d7604b37fb9
iamidlek <iamidlek@naver.com>
2022-03-15 14:16:38 +0900

    feat: use multer for upload file

--- chatapp-react/back/index.js
+++ chatapp-react/back/index.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -16,15 +18,36 @@ dotenv.config();
 mongoose.connect(
   process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
+  (err) => {
+    if (err) console.log(err);
+    else console.log("mongdb is connected");
   }
 );
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
