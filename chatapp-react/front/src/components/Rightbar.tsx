569ff375b733748d4d72ccc265332b271296a9a1
iamidlek <iamidlek@naver.com>
2022-03-16 21:28:04 +0900

    feat: get props about user info

--- chatapp-react/front/src/components/Rightbar.tsx
+++ chatapp-react/front/src/components/Rightbar.tsx
@@ -1,12 +1,19 @@
+import { IUser } from 'src/context/interface';
 import styled from 'styled-components';
 import HomeRightbar from './HomeRightbar';
 import ProfileRightbar from './ProfileRightbar';
 
-export default function Rightbar({ profile = false }) {
+export default function Rightbar({
+  profile = false,
+  user,
+}: {
+  profile?: boolean;
+  user?: IUser;
+}) {
   return (
     <Rbar>
       <RbarWrapper>
-        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+        {user && profile ? <ProfileRightbar user={user} /> : <HomeRightbar />}
       </RbarWrapper>
     </Rbar>
   );
