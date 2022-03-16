c3b8e34852e240847d59bd25f3ec2128c2f6d793
iamidlek <iamidlek@naver.com>
2022-03-16 21:42:04 +0900

    feat: get posts from mongo

--- chatapp-react/front/src/components/Feed.tsx
+++ chatapp-react/front/src/components/Feed.tsx
@@ -1,15 +1,39 @@
 import Post from './Post';
 import Share from './Share';
-import { Posts } from '../dummyData';
 import styled from 'styled-components';
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
+import { Ipost } from 'src/context/interface';
 
-export default function Feed() {
+export default function Feed({ username }: { username?: string }) {
+  const [posts, setPosts] = useState<Ipost[]>();
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const { data } = username
+        ? await axios.get('/api/posts/profile/' + username)
+        : await axios.get('/api/posts/timeline/' + user?._id);
+      setPosts(
+        (data as Ipost[]).sort((p1, p2) => {
+          if (p2.createdAt && p1.createdAt) {
+            const a = new Date(p1.createdAt);
+            const b = new Date(p2.createdAt);
+            return a > b ? -1 : a < b ? 1 : 0;
+          }
+          return 0;
+        }),
+      );
+    };
+    fetchPosts();
+  }, [username, user?._id]);
   return (
     <Feeds>
       <FeedWrapper>
-        <Share />
-        {Posts.map((post) => (
-          <Post key={post.id} post={post} />
+        {(!username || username === user?.username) && <Share />}
+        {posts?.map((post) => (
+          <Post key={post._id} post={post} />
         ))}
       </FeedWrapper>
     </Feeds>
