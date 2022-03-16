1c46b94414791441e8bb189a2196f23ca9aa85af
iamidlek <iamidlek@naver.com>
2022-03-16 09:58:17 +0900

    feat: check user

--- chatapp-react/front/src/pages/Home.tsx
+++ chatapp-react/front/src/pages/Home.tsx
@@ -1,10 +1,22 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { AuthContext } from '../context/AuthContext';
+
 import Topbar from '../components/Topbar';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
 import Rightbar from '../components/Rightbar';
 
 const Home = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user]);
   return (
     <>
       <Topbar />
