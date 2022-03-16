d20e1fc9ebae516e06321b76717b6b65df1a7723
iamidlek <iamidlek@naver.com>
2022-03-16 21:24:14 +0900

    feat: get user from mongo

--- chatapp-react/front/src/pages/Profile.tsx
+++ chatapp-react/front/src/pages/Profile.tsx
@@ -1,31 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import styled from 'styled-components';
+import { IUser } from '../context/interface';
 import Feed from '../components/Feed';
 import Rightbar from '../components/Rightbar';
 import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
 
 export default function Profile() {
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState<IUser>();
+  const { username } = useParams();
+
+  const fetchUser = async () => {
+    const { data } = await axios.get(`/api/users?username=${username}`);
+    setUser(data);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, [username]);
   return (
     <>
       <Topbar />
       <ProfileBox>
         <Sidebar />
-        <div>
+        <ProfileRight>
           <ProfileRightTop>
             <ProfileCover>
-              <ProfileCoverImg src="assets/post/3.jpeg" alt="" />
-              <ProfileUserImg src="assets/person/7.jpeg" alt="" />
+              <ProfileCoverImg
+                src={
+                  user?.coverPicture
+                    ? PF + user.coverPicture
+                    : PF + 'person/noCover.png'
+                }
+                alt=""
+              />
+              <ProfileUserImg
+                src={
+                  user?.profilePicture
+                    ? PF + user.profilePicture
+                    : PF + 'person/noAvatar.png'
+                }
+                alt=""
+              />
             </ProfileCover>
             <ProfileInfo>
-              <ProfileInfoName>Safak Kocaoglu</ProfileInfoName>
-              <ProfileInfoDesc>Hello my friends!</ProfileInfoDesc>
+              <ProfileInfoName>{user?.username}</ProfileInfoName>
+              <ProfileInfoDesc>{user?.desc}</ProfileInfoDesc>
             </ProfileInfo>
           </ProfileRightTop>
           <ProfileRightBottom>
-            <Feed />
-            <Rightbar profile />
+            <Feed username={username} />
+            {user && <Rightbar user={user} />}
           </ProfileRightBottom>
-        </div>
+        </ProfileRight>
       </ProfileBox>
     </>
   );
@@ -33,6 +63,10 @@ export default function Profile() {
 
 const ProfileBox = styled.div`
   display: flex;
+`;
+
+const ProfileRight = styled.div`
+  flex: 9;
 `;
 
 const ProfileRightTop = styled.div`
