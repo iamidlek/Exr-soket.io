4a8a2377281c8bac3bde188a0b0a91ce3c3ca6ae
iamidlek <iamidlek@naver.com>
2022-03-17 15:17:45 +0900

    feat: fix api url

--- chatapp-react/front/src/components/ProfileRightbar.tsx
+++ chatapp-react/front/src/components/ProfileRightbar.tsx
@@ -19,12 +19,12 @@ export default function ProfileRightbar({ user }: { user: IUser }) {
     try {
       if (dispatch) {
         if (followed) {
-          await axios.put(`/users/${user._id}/unfollow`, {
+          await axios.put(`/api/users/${user._id}/unfollow`, {
             userId: currentUser?._id,
           });
           dispatch({ type: 'UNFOLLOW', payload: user._id as string });
         } else {
-          await axios.put(`/users/${user._id}/follow`, {
+          await axios.put(`/api/users/${user._id}/follow`, {
             userId: currentUser?._id,
           });
           dispatch({ type: 'FOLLOW', payload: user._id as string });
@@ -37,8 +37,8 @@ export default function ProfileRightbar({ user }: { user: IUser }) {
   useEffect(() => {
     const getFriends = async () => {
       try {
-        const friendList = await axios.get('/api/users/friends/' + user._id);
-        setFriends(friendList.data);
+        const { data } = await axios.get('/api/users/friends/' + user._id);
+        setFriends(data);
       } catch (err) {
         console.log(err);
       }
