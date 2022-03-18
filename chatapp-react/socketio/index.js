a9148db2b31bc9489f19dfabe46528912f218771
iamidlek <iamidlek@naver.com>
2022-03-18 22:46:55 +0900

    feat: add, remove user

--- chatapp-react/socketio/index.js
+++ chatapp-react/socketio/index.js
@@ -9,3 +9,12 @@ let users = [];
 const getUser = (userId) => {
   return users.find((user) => user.userId === userId);
 };
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
