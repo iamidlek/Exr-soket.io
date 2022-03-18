9dca8d393838464dc04197aaf56c8ac0fbd6c020
iamidlek <iamidlek@naver.com>
2022-03-18 22:46:28 +0900

    feat: create users and getuser function

--- chatapp-react/socketio/index.js
+++ chatapp-react/socketio/index.js
@@ -3,3 +3,9 @@ const io = require("socket.io")(8900, {
     origin: "http://localhost:3000",
   },
 });
+
+let users = [];
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
